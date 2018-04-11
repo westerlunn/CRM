191c1088b89c4345e4938021a9da4599bc811f25
Amanda Westerlund <Westerlund922@gmail.com>
2018-04-11 11:33:31 +0200

    Val för användare tillagt.

--- CRM/CRM/Program.cs
+++ CRM/CRM/Program.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.Data.SqlClient;
 using System.Security.Cryptography.X509Certificates;
+using ConsoleTableExt;
 
 namespace CRM
 {
@@ -11,10 +12,12 @@ namespace CRM
 
         static void Main(string[] args)
         {
-            GetDatabase.UpdateCustomer(5, "Anton", "Oq", "anoq@gmail.com", "0708909876");
+            //GetDatabase.GetCustomers();
+            GetDatabase.Menu();
+            //GetDatabase.UpdateCustomer(5, "Anton", "Oq", "anoq@gmail.com", "0708909876");
             //GetDatabase.RemoveCustomer(6);
             //GetDatabase.CreateCustomer("Karl", "Blå", "hejhopp@plupp.se", "0709876543");
-            GetDatabase.GetInfoFromDatabase();
+            //GetDatabase.GetInfoFromDatabase();
         }
 
     }
@@ -46,6 +49,92 @@ namespace CRM
                     System.Console.WriteLine($"Name : {firstName} {lastName}");
                     System.Console.WriteLine($"Contact information: {email}, {phoneNumber}");
                 }
+            }
+        }
+
+        public static void Menu()
+        {
+            while (true)
+            {
+                Console.WriteLine("1: View customers \n2: Create customer \n3: Remove customer \n4: Update customer");
+                var userChoice = Console.ReadLine();
+
+                if (userChoice == "1")
+                {
+                    GetCustomers();
+                }
+
+                if (userChoice == "2")
+                {
+                    Console.Write("First name: ");
+                    var firstName = Console.ReadLine();
+                    Console.Write("Last name: ");
+                    var lastName = Console.ReadLine();
+                    Console.Write("Email: ");
+                    var mail = Console.ReadLine();
+                    Console.Write("Phone number: ");
+                    var number = Console.ReadLine();
+                    CreateCustomer(firstName, lastName, mail, number);
+                }
+
+                if (userChoice == "3")
+                {
+                    Console.WriteLine("Enter the ID of the customer you want to remove: ");
+                    var id = Convert.ToInt32(Console.ReadLine());
+                    RemoveCustomer(id);
+                }
+
+                if (userChoice == "4")
+                {
+                    Console.Write("Customer ID: ");
+                    var id = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("First name: ");
+                    var firstName = Console.ReadLine();
+                    Console.Write("Last name: ");
+                    var lastName = Console.ReadLine();
+                    Console.Write("Email: ");
+                    var mail = Console.ReadLine();
+                    Console.Write("Phone number: ");
+                    var number = Console.ReadLine();
+
+                    UpdateCustomer(id, firstName, lastName, mail, number);
+                }
+
+                if (userChoice == "5")
+                    break;
+            }
+        }
+
+        public static void GetCustomers()
+        {
+            var sql = @"SELECT [ID], [firstName], [lastName], [email], [phoneNumber] FROM Customer";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                using (DataTable dataTable = new DataTable())
+                {
+                    dataTable.TableName = "customers";
+                    dataTable.Columns.Add("ID", typeof(string));
+                    dataTable.Columns.Add("Name", typeof(string));
+                    dataTable.Columns.Add("Email", typeof(string));
+                    dataTable.Columns.Add("Number", typeof(string));
+
+                    while (reader.Read())
+                    {
+                        dataTable.Rows.Add(reader.GetInt32(0), reader.GetString(1) + " " + reader.GetString(2), reader.GetString(3), reader
+                            .GetString(4));
+                    }
+                    ConsoleTableBuilder
+                        .From(dataTable)
+                        .WithFormat(ConsoleTableBuilderFormat.Minimal)
+                        .ExportAndWriteLine();
+                }
+                   
             }
         }
 
